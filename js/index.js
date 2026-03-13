4307f1bf1aa476c9f8c9425f9c3fa32656325808
Muhammad Ramzan <ramzanlynx@tuta.io>
2026-03-13 14:50:05 +0500

    add amount sum functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // import all the javascript modules
 
+import "./cart.js";
 import "./wizard.js";
 import "./password.js";
 import "./cart/counter.js";
 import "./setup-ui.js";
+import "./cart/price.js";
